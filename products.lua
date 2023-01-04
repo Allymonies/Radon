e6ebbc2af285de7bf6254bc1a60e11d17e4fa67e
Alyssa May <may@alyss.am>
2023-01-04 12:07:12 -0800

    Better example products file

--- products.lua
+++ products.lua
@@ -1,5 +1,11 @@
 return {
     {
+        modid = "minecraft:stick",
+        name = "Stick",
+        address = "stick",
+        price = 0.1
+    },
+    {
         modid = "minecraft:lapis_block",
         name = "Lapis Block",
         address = "lapis",
@@ -7,34 +13,8 @@ return {
         price = 9.0,
         priceOverrides = {
             {
-                currency = "tenebra",
+                currency = "krist",
                 price = 18.0
-            }
-        },
-    },
-    {
-        modid = "minecraft:diamond",
-        name = "Diamond",
-        address = "dia",
-        category = "ore",
-        price = 5.0,
-        priceOverrides = {
-            {
-                currency = "tenebra",
-                price = 35.0
-            }
-        },
-    },
-    {
-        modid = "minecraft:gold_ingot",
-        name = "Gold Ingot",
-        address = "gold",
-        category = "ore",
-        price = 5.0,
-        priceOverrides = {
-            {
-                currency = "tenebra",
-                price = 35.0
             }
         },
     },
@@ -51,11 +31,5 @@ return {
                 }
             }
         }
-    },
-    {
-        modid = "scgoodies:lesbian_elytra",
-        name = "Lesbian Elytra",
-        address = "lely",
-        price = 5000.0
     }
 }
