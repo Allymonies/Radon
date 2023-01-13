c286ca05ac701c4af61d3508004d4111fa3619a6
Alyssa May <may@alyss.am>
2023-01-12 23:45:09 -0800

    Bump version

--- radon.lua
+++ radon.lua
@@ -1,4 +1,4 @@
-local version = "1.3.11"
+local version = "1.3.12"
 local configHelpers = require "util.configHelpers"
 local schemas       = require "core.schemas"
 local oldPullEvent = os.pullEvent
